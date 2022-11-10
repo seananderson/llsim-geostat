cbed7f6730714ae094809cfc26dd66aaac3afa2e
Sean Anderson <sean@seananderson.ca>
2022-11-09 23:28:48 -0800

    Catch up

--- make.R
+++ make.R
@@ -1,52 +1,81 @@
 rmarkdown::render("geostat.Rmd",
   params = list(
     use_sets = TRUE,
-    fleets = c(1),
-    species = "swo",
-    spatiotemporal = "iid"
-  ),
-  output_file = "us-sets-iid-swo.html"
-)
-
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
-    fleets = c(1),
-    species = "bum",
-    spatiotemporal = "off"
-  ),
-  output_file = "us-sets-bum.html"
-)
-
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
-    fleets = c(1, 2, 3),
-    species = "swo",
-    spatiotemporal = "off"
-  ),
-  output_file = "all-sets-swo.html"
-)
-
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
-    fleets = c(1, 2, 3),
-    species = "bum",
-    spatiotemporal = "iid",
-    spatial = "on"
-  ),
-  output_file = "all-sets-bum-iid.html"
-)
-
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
     fleets = c(1, 2, 3),
     species = "bum",
     spatiotemporal = "iid",
     spatial = "on",
-    share_range = FALSE
+    share_range = FALSE,
+    cache_path = "all-sets-bum-iid-share-range-false"
   ),
   output_file = "all-sets-bum-iid-share-range-false.html"
+)
+
+rmarkdown::render("geostat.Rmd",
+  params = list(
+    use_sets = TRUE,
+    fleets = c(1, 2, 3),
+    species = "bum",
+    spatiotemporal = "off",
+    spatial = "off",
+    share_range = TRUE,
+    cache_path = "all-sets-bum-no-fields",
+    anisotropy = FALSE
+  ),
+  output_file = "all-sets-bum-no-fields.html"
+)
+
+rmarkdown::render("geostat.Rmd",
+  params = list(
+    use_sets = TRUE,
+    fleets = c(1, 2, 3),
+    species = "bum",
+    spatiotemporal = "off",
+    spatial = "on",
+    share_range = TRUE,
+    cache_path = "all-sets-bum-spatial-only",
+    anisotropy = TRUE
+  ),
+  output_file = "all-sets-bum-spatial-only.html"
+)
+
+rmarkdown::render("geostat.Rmd",
+  params = list(
+    use_sets = TRUE,
+    fleets = c(1),
+    species = "bum",
+    spatiotemporal = "iid",
+    spatial = "on",
+    share_range = FALSE,
+    cache_path = "us-sets-bum-iid-share-range-false"
+  ),
+  output_file = "us-sets-bum-iid-share-range-false.html"
+)
+
+rmarkdown::render("geostat.Rmd",
+  params = list(
+    use_sets = TRUE,
+    fleets = c(1),
+    species = "bum",
+    spatiotemporal = "off",
+    spatial = "off",
+    share_range = TRUE,
+    cache_path = "us-sets-bum-no-fields",
+    anisotropy = FALSE
+  ),
+  output_file = "us-sets-bum-no-fields.html"
+)
+
+rmarkdown::render("geostat.Rmd",
+  params = list(
+    use_sets = TRUE,
+    fleets = c(1),
+    species = "bum",
+    spatiotemporal = "off",
+    spatial = "on",
+    share_range = TRUE,
+    cache_path = "us-sets-bum-spatial-only",
+    anisotropy = TRUE
+  ),
+  output_file = "us-sets-bum-spatial-only.html"
 )
