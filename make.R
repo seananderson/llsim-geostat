42d835afe952ba1895d7769af7860fbd981a6b3b
Sean Anderson <sean@seananderson.ca>
2023-06-01 16:33:34 -0700

    Tidy

--- make.R
+++ make.R
@@ -39,43 +39,45 @@ rmarkdown::render("geostat.Rmd",
   output_file = "all-sets-bum-spatial-only.html"
 )
 
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
-    fleets = c(1),
-    species = "bum",
-    spatiotemporal = "iid",
-    spatial = "on",
-    share_range = FALSE,
-    cache_path = "us-sets-bum-iid-share-range-false"
-  ),
-  output_file = "us-sets-bum-iid-share-range-false.html"
-)
+# US only:
 
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
-    fleets = c(1),
-    species = "bum",
-    spatiotemporal = "off",
-    spatial = "off",
-    share_range = TRUE,
-    cache_path = "us-sets-bum-no-fields",
-    anisotropy = FALSE
-  ),
-  output_file = "us-sets-bum-no-fields.html"
-)
-
-rmarkdown::render("geostat.Rmd",
-  params = list(
-    use_sets = TRUE,
-    fleets = c(1),
-    species = "bum",
-    spatiotemporal = "off",
-    spatial = "on",
-    share_range = TRUE,
-    cache_path = "us-sets-bum-spatial-only",
-    anisotropy = TRUE
-  ),
-  output_file = "us-sets-bum-spatial-only.html"
-)
+# rmarkdown::render("geostat.Rmd",
+#   params = list(
+#     use_sets = TRUE,
+#     fleets = c(1),
+#     species = "bum",
+#     spatiotemporal = "iid",
+#     spatial = "on",
+#     share_range = FALSE,
+#     cache_path = "us-sets-bum-iid-share-range-false"
+#   ),
+#   output_file = "us-sets-bum-iid-share-range-false.html"
+# )
+#
+# rmarkdown::render("geostat.Rmd",
+#   params = list(
+#     use_sets = TRUE,
+#     fleets = c(1),
+#     species = "bum",
+#     spatiotemporal = "off",
+#     spatial = "off",
+#     share_range = TRUE,
+#     cache_path = "us-sets-bum-no-fields",
+#     anisotropy = FALSE
+#   ),
+#   output_file = "us-sets-bum-no-fields.html"
+# )
+#
+# rmarkdown::render("geostat.Rmd",
+#   params = list(
+#     use_sets = TRUE,
+#     fleets = c(1),
+#     species = "bum",
+#     spatiotemporal = "off",
+#     spatial = "on",
+#     share_range = TRUE,
+#     cache_path = "us-sets-bum-spatial-only",
+#     anisotropy = TRUE
+#   ),
+#   output_file = "us-sets-bum-spatial-only.html"
+# )
